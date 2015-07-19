0ae1862fb112130ec79754bcece42e05765914ab
Sandeep Panda <sandeep@thexprs.com>
2015-07-20 00:15:58 +0530

    made header.jsx uppercase while requiring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 var React = require('react/addons');
 var RouteHandler = require('react-router').RouteHandler;
 var Link = require('react-router').Link;
-var Header = require('./header.jsx');
+var Header = require('./Header.jsx');
 
 var App = React.createClass({
 
